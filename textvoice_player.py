7149eb6f2b2d9285a941e092cca355053cfa1b1a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-05-27 20:22:29 +0200

    Update textvoice_player.py

--- textvoice_player.py
+++ textvoice_player.py
@@ -1,8 +1,15 @@
 import winspeech
 print("TEXT_VOICE")
-print("ENTER \'.\' TO FINISH THE PROGRAM")
+print("ENTER \'.\' TO FINISH THE PROGRAM AND SPACE (\' \') TO REPIT THE PREVIOUS MESSAGE")
 texto=("")
+prev_text=("")
 while texto!=("."):
     texto=input("Your text: ")
-    winspeech.say(texto)
-    
+    if texto==(" "):
+        if prev_text==(""):
+            print(chr(7),"THERE'S NOT A PREVIOUS MESSAGE")
+        else:
+            winspeech.say(prev_text)
+    else:
+        winspeech.say(texto)
+        prev_text=texto
