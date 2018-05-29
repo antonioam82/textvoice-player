887931a07db21968471197decb81aa5ddd26a7e6
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-05-29 23:50:11 +0200

    Update textvoiceBIS.py

--- textvoiceBIS.py
+++ textvoiceBIS.py
@@ -3,7 +3,7 @@ print("REPRODUCCIÓN DE TEXTO MEDIANTE VOZ")
 print("INTRODUCE \'.\' PARA FINALIZAR EL PROGRAMA Y ESPACIO (\' \') PARA REPETIR EL ÚLTIMO MENSAGE INTRODUCIDO/REPRODUCIDO")
 texto=("")
 prev_text=("")
-speak=wc.Dispatch("Sapi.SpVoice")
+voice=wc.Dispatch("Sapi.SpVoice")#Llamar al método Dispatch del módulo
 while texto!=("."): #MIENTRAS QUE "texto" SEA DISTINTO A "."
     texto=input("TU TEXTO: ")
     if texto==(" "):
@@ -11,7 +11,8 @@ while texto!=("."): #MIENTRAS QUE "texto" SEA DISTINTO A "."
             print("NO HAY MENSAGE ANTERIOR",chr(7))
         else:
             print("REPITIENDO MENSAGE")
-            speak.Speak(prev_text)
+            voice.Speak(prev_text)
     else:
-        speak.Speak(texto)
+        voice.Speak(texto)
         prev_text=texto
+    
