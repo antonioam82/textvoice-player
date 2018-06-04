61e3602150c3c57cd5d30df71ffc3bfbdc1b1cf8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-06-04 10:34:14 +0200

    Update textvoiceBIS.py

--- textvoiceBIS.py
+++ textvoiceBIS.py
@@ -6,6 +6,8 @@ prev_text=("")
 voice=wc.Dispatch("Sapi.SpVoice")#Llamar al método Dispatch del módulo
 while texto!=("."): #MIENTRAS QUE "texto" SEA DISTINTO A "."
     texto=input("TU TEXTO: ")
+    texto=texto.lower()
+    print(texto)
     if texto==(" "):
         if prev_text==(""):
             print("NO HAY MENSAGE ANTERIOR",chr(7))
@@ -15,4 +17,5 @@ while texto!=("."): #MIENTRAS QUE "texto" SEA DISTINTO A "."
     else:
         voice.Speak(texto)
         prev_text=texto
+    
     
