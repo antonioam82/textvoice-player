176dac17695719b3e9d6ca3427d1a1b4e46c002c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-09-06 19:10:08 +0200

    Update textvoice_player.py

--- textvoice_player.py
+++ textvoice_player.py
@@ -1,15 +1,15 @@
 import winspeech
 print("TEXT_VOICE")
-print("INTRODUCE \'.\' PARA FINALIZAR EL PROGRAMA Y ESPACIO (\' \') PARA REPETIR EL ÚLTIMO MENSAGE INTRODUCIDO/REPRODUCIDO")
+print("INTRODUCE \'.\' PARA FINALIZAR EL PROGRAMA Y ESPACIO (\' \') PARA REPETIR EL ÚLTIMO MENSAJE INTRODUCIDO/REPRODUCIDO")
 texto=("")
 prev_text=("")
 while texto!=("."):
     texto=input("TU TEXTO EN INGLÉS: ")
     if texto==(" "):
         if prev_text==(""):
-            print(chr(7),"NO HAY MENSAGE ANTERIOR")
+            print(chr(7),"NO HAY MENSAJE ANTERIOR")
         else:
-            print("REPITIENDO MENSAGE")
+            print("REPITIENDO MENSAJE")
             winspeech.say(prev_text)
     else:
         winspeech.say(texto)
