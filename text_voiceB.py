fcb24d357a31ff1c5fea7e7b307450c79345255d
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-02-19 12:42:42 +0100

    Create text_voiceB.py

---
+++ text_voiceB.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import win32com.client as wc
+
+print(" _________")
+print("|___   ___|")
+print("    | | _____        _____               ______  __  ______  _____  ")
+print("    | ||  ___|/\  /\|_   _|   /\      /\|  __  ||  ||  ____||  ___| ")
+print("    | || |    \ \/ /  | | ____\ \    / /| |  | ||  || |     | |     ")
+print("    | || >>>   \  /   | ||    |\ \  / / | |  | ||  || |     | >>>   ")
+print("    | || |___  /  \   | ||____| \ \/ /  | |__| ||  || |____ | |___  ")
+print("    |_||_____|/_/\_\  |_|        \__/   |______||__||______||_____| ")
+print("---------------------------------------------------------------------")
+print("---------------------------------------------------------------------")
+print("")
+print("""INTRODUCE \'.\' PARA FINALIZAR EL PROGRAMA Y ESPACIO (\' \') PARA REPETIR
+EL ÚLTIMO MENSAJE INTRODUCIDO/REPRODUCIDO.""")
+print("")
+t=("")
+prev_text=("")
+speak=wc.Dispatch("Sapi.SpVoice")
+
+def main_func(texto):
+    while texto!=("."): #MIENTRAS QUE "texto" SEA DISTINTO A "."
+        texto=input("TU TEXTO: ")
+        if texto==(" "):
+            if prev_text==(""):
+                print("NO HAY MENSAJE ANTERIOR",chr(7))
+            else:
+                print("REPITIENDO MENSAJE")
+                speak.Speak(prev_text)
+        else:
+            speak.Speak(texto)
+            prev_text=texto
+
+if __name__=="__main__":
+    main_func(t)
