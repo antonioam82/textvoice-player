be565a1a200d0f317eaf34c3ddda1ee5d166e8d2
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-10 14:45:34 +0200

    Update text_voiceNEW.py

--- text_voiceNEW.py
+++ text_voiceNEW.py
@@ -2,7 +2,9 @@ import pyttsx3
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
+engine.setProperty('rate',180)
 text = ""
+prev_text=""
 
 print("*ESTABLECE IDIOMA*")
 print("A)-ESPAÑOL--------")
@@ -18,7 +20,31 @@ while True:
         break
     print("Intrpduzca \'A\' o \'B\' según su opción")
 
+print(" _________")
+print("|___   ___|")
+print("    | | _____        _____               ______  __  ______  _____  ")
+print("    | ||  ___|/\  /\|_   _|   /\      /\|  __  ||  ||  ____||  ___| ")
+print("    | || |    \ \/ /  | | ____\ \    / /| |  | ||  || |     | |     ")
+print("    | || >>>   \  /   | ||    |\ \  / / | |  | ||  || |     | >>>   ")
+print("    | || |___  /  \   | ||____| \ \/ /  | |__| ||  || |____ | |___  ")
+print("    |_||_____|/_/\_\  |_|        \__/   |______||__||______||_____| ")
+print("---------------------------------------------------------------------")
+print("---------------------------------------------------------------------\n")
+
+def speak(m):
+    engine.say(m)
+    engine.runAndWait()
+
 while text != ".":
     text = input("TEXT: ")
-    engine.say(text)
-    engine.runAndWait()
+    if text == " ":
+        if prev_text == "":
+            print("NO HAY MENSAJE ANTERIOR",chr(7))
+        else:
+            print("REPITIENDO MENSAJE")
+            speak(prev_text)
+            
+    else:
+        speak(text)
+        prev_text = text
+
